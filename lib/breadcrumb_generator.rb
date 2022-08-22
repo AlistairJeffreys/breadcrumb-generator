ec1cc863c09a43cdeb0e17306e77d1a4bba41de4
Alistair Jeffreys <alistair.jeffreys@bt.com>
2022-08-22 23:23:40 +0100

    Add breadcrumb_function will nil checking tests

--- lib/breadcrumb_generator.rb
+++ lib/breadcrumb_generator.rb
@@ -1,3 +1,7 @@
 class BreadcrumbGenerator
-  
+
+  def breadcrumb_function url, separator
+
+  end
+
 end
--- spec/breadcrumb_generator_spec.rb
+++ spec/breadcrumb_generator_spec.rb
@@ -1,5 +1,28 @@
 require "./lib/breadcrumb_generator"
 
 describe BreadcrumbGenerator do
+  subject { described_class.new }
+
+  describe ".breadcrumb_function" do
+    context "when the arguments are invalid" do
+      context "when both arguments are nil" do
+        it "returns nil" do
+          expect(subject.breadcrumb_function(nil, nil))
+        end
+      end
+
+      context "when the first argument is nil" do
+        it "returns nil" do
+          expect(subject.breadcrumb_function(nil, " : "))
+        end
+      end
+
+      context "when the second argument is nil" do
+        it "returns nil" do
+          expect(subject.breadcrumb_function("https://www.btplc.com/SITES/ADASTRAL.HTML", nil))
+        end
+      end
+    end
+  end
 
 end
