88f05e1e6d001d1719e3b13c52288d8114e8353b
Alistair Jeffreys <alistair.jeffreys@bt.com>
2022-08-22 23:30:14 +0100

    Add the returning of HOME wrapped in a tags

--- lib/breadcrumb_generator.rb
+++ lib/breadcrumb_generator.rb
@@ -1,7 +1,9 @@
 class BreadcrumbGenerator
 
   def breadcrumb_function url, separator
+    return unless url && separator
 
+    '<a href="/">HOME</a>'
   end
 
 end
--- spec/breadcrumb_generator_spec.rb
+++ spec/breadcrumb_generator_spec.rb
@@ -11,18 +11,31 @@ describe BreadcrumbGenerator do
         end
       end
 
-      context "when the first argument is nil" do
+      context "when the url is nil" do
         it "returns nil" do
           expect(subject.breadcrumb_function(nil, " : "))
         end
       end
 
-      context "when the second argument is nil" do
+      context "when the seperator is nil" do
         it "returns nil" do
           expect(subject.breadcrumb_function("https://www.btplc.com/SITES/ADASTRAL.HTML", nil))
         end
       end
     end
-  end
 
+    context "with a colon seperator" do
+      let(:seperator) { " : " }
+
+      context "when given only the base url" do
+        context "without a trailing /" do
+          let(:url) { "https://www.btplc.com" }
+
+          it "returns HOME surrounded by an href tag" do
+            expect(subject.breadcrumb_function(url, seperator)).to eq '<a href="/">HOME</a>'
+          end
+        end
+      end
+    end
+  end
 end
